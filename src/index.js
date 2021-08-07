f5908216506cc79089cbcf17cdf4536227500f63
Evgeny Kolotov <e.Kolotov1995@gmail.com>
2021-08-08 00:20:55 +0300

    Инициализированно подключение к БД

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ const config = require('config');
 
 const application = express();
 const PORT = config.get("serverPort");
+const MONGO_URL = config.get("MongoURL"); 
 
-const startServer = () => {
+const startServer = async () => {
     try {
+        mongoose.connect(MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
         application.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
