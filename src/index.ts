0bf1333f38b97f220ee1a8cc0645dcffd3576c2b
Evgeny Kolotov <e.Kolotov1995@gmail.com>
2021-08-25 22:54:49 +0300

    Инициализация конфига для typescript

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const config = require("config");
-const fileUpload = require("express-fileupload");
+import express from "express";
+import mongoose from "mongoose";
+/*import config from "config";
+import fileUpload from "express-fileupload";
+import authRouter from "./routes/auth.routes";
+import fileRouter from "./routes/file.routes";*/
 
 const application = express();
 const PORT = config.get("serverPort");
-const MONGO_URL = config.get("MongoURL");
+//const MONGO_URL = config.get("MongoURL");
 
-const authRouter = require("./routes/auth.routes");
-const fileRouter = require("./routes/file.routes");
-
+/*
 application.use(fileUpload({}));
 application.use(express.json());
 application.use("/api/auth", authRouter);
 application.use("/api/file", fileRouter);
-
+*/
 const startServer = async () => {
     try {
-        mongoose.connect(MONGO_URL, {
+        /*mongoose.connect(MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
             useFindAndModify: false,
-        });
+        });*/
         application.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
